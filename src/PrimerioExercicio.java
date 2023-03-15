600560618d5b64c77afb49023ff5f137ee57bb7d
Snaymi <sneymeb@hotmail.com>
2023-03-15 12:37:45 -0300

    FATORIAL

--- src/PrimerioExercicio.java
+++ src/PrimerioExercicio.java
@@ -1,16 +1,43 @@
+import java.util.Scanner;
+
+
 public class PrimerioExercicio {
+    public static void main (String[] args) {
 
+        int n1;
+        
+        int resultado;
+        String res;
+        Scanner scan = new Scanner (System.in);
+        
+        
+        do {
+            System.out.println("Vamos ver a tabuada de qual número? ");
+            int count = 1;
+            n1 = scan.nextInt();
+            if (n1>=1 && n1<=10) {
 
-        public static void main (String[] args) {
-            int a = 1, b = 2, c = 3, d = 4, e = 5;
+                while (count<11){
             
-            String resultado = a>b ? "A é maior que B ": "A é menor que B";
+                    resultado = n1 * count;
 
-            System.out.println("Parece que " + resultado);
+                    
 
-            String teste = a+b+d ==7 ? "confirma" : "não confirma";
+                    System.out.println(n1 +" x " + count + " = " + resultado);
+                    
+                    count = count+1;
+            }
+                }else {
+                System.out.println("Número inválido");
+            }
+        
+        System.out.println("Gostaria de colocar outro número? S/N");
+    
+        res = scan.next();
+        
+        } while (res.equals ("s") || res.equals ("S"));
 
-            System.out.println("Aparentemente " +teste);
     }
-
 }
+
+   
