e33b22944e7db9cd857c7da2e47c1d6b5cc860f9
Snaymi <sneymeb@hotmail.com>
2023-03-15 10:31:57 -0300

    Tabuada até 10

---
+++ src/PrimerioExercicio.java
@@ -0,0 +1,16 @@
+public class PrimerioExercicio {
+
+
+        public static void main (String[] args) {
+            int a = 1, b = 2, c = 3, d = 4, e = 5;
+            
+            String resultado = a>b ? "A é maior que B ": "A é menor que B";
+
+            System.out.println("Parece que " + resultado);
+
+            String teste = a+b+d ==7 ? "confirma" : "não confirma";
+
+            System.out.println("Aparentemente " +teste);
+    }
+
+}
